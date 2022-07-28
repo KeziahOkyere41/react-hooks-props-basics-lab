1dcd2fdb17d1f76639112d4079fc4a81a96aedc2
KeziahOkyere41 <Nharnhayhaar208@gmail.com>
2022-07-28 20:34:58 +0000

    completed assignment

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import NavBar from "./NavBar";
 import Home from "./Home";
 import About from "./About";
-import Links from "./Links";
 
 // pass this data down as props to the child component(s) that need it!
 import user from "../data/user";
@@ -12,8 +11,8 @@ function App() {
   return (
     <div>
       <NavBar />
-      <Home name='Liza' city='New York' />
-      <About bio='I made this' />
+      <Home name={user.name} city={user.city} color = {user.color} />
+      <About bio={user.bio} />
     </div>
   );
 }
