47d86dca03cb80264503e53cd62c0b7c2cd22bb4
KeziahOkyere41 <Nharnhayhaar208@gmail.com>
2022-07-29 00:16:15 +0000

    completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,6 @@
 import React from "react";
+import user from "../data/user";
+import Links from "./Links";
 
 function About(props) {
   if(!props.bio){
@@ -7,8 +9,9 @@ function About(props) {
 return (
   <div id="about">
     <h2>About Me</h2>
-    <p>Props.bio</p>
+    <p>{props.bio}</p>
     <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
+    <Links github ={user.links.github} linkedin = {user.links.linkedin}/>
   </div>
 );
 }
